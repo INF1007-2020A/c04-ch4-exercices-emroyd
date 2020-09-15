e02074456f7dcf741958f1ac7fa3c926ad34c534
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-15 23:31:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,7 +13,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     premiers=string[0:6]
-    remplacer='w'
+    remplacer='z'
     derniers=string[7:len(string)]
     return(premiers+remplacer+derniers)
         
