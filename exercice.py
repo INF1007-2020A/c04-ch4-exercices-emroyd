a1eb7b050c3d3edbdb106864009288e08c014f77
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-15 22:48:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -7,7 +7,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    return (for c in [0:2,4:len(string)])
+    return (for c in ([0:2,4:len(string)])
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
