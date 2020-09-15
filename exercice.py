e4a73a151575f1634154a4c8f1758c40f2cee239
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-15 23:32:01 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,13 +10,13 @@ def remove_third_char(string: str) -> str:
     derniers=string[3:len(string)]
     return (premiers+derniers)
 
-
+#Option 1
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     premiers=string[0:6]
     remplacer='z'
     derniers=string[7:len(string)]
     return(premiers+remplacer+derniers)
-        
+%      
 
 
 def get_number_of_char(string: str, char: str) -> int:
