d1137562b6808c44ad54c4c6fe794b62115fc9da
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-15 23:31:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
     premiers=string[0:6]
     remplacer='w'
     derniers=string[7:len(string)]
-    return(i)
+    return(premiers+remplacer+derniers)
         
 
 
