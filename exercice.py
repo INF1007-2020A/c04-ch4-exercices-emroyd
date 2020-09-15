92bdabf0558b15bbb68fb798dc4ce7e223b713ed
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-15 23:23:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,7 @@ def remove_third_char(string: str) -> str:
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    if 
+    if string==
 
 
 def get_number_of_char(string: str, char: str) -> int:
