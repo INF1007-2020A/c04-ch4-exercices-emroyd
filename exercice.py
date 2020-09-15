13e6505092c04b322f5cc402b6e0e2dfe021df55
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-15 23:34:54 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def get_number_of_char(string: str, char: str) -> int:
     char=1
     if char in string
         return(True)
-        
+    else:
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
