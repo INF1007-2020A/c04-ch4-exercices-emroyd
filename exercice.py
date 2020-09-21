186d74bc209694618119e500454f2efa7165595c
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-21 22:26:26 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,8 +4,8 @@
 
 def is_even_len(string: str) -> bool:
     nbr=len(string)
-    nbr=nbr/2
-    if nbr = int(nbr/2):
+    nbr= nbr/2
+    if int(nbr):
     print('Le nombre de lettre est pair')
     else:
     print('Le nombre de lettre est impair')
