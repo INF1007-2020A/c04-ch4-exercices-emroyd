7ccd93d11f0f4349cb36dd3b3c87f7636cb40d8a
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-21 22:21:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@
 def is_even_len(string: str) -> bool:
     nbr=len(string)
     if nbr = int(nbr/2)
-    print('')
+    print('Le nombre de lettre est pair')
 
 def remove_third_char(string: str) -> str:
     premiers=string[0:2]
